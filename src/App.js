21269f6a019a7b52a02565da1fc189902f2fae36
jort2 <>
2023-03-23 18:05:49 +0100

    Creating routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
+import{createBrowserRouter, RouterProvider, Route} from 'react-router-dom';
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Write from './pages/Write'
+import Single from './pages/Single'
+
+const router = createBrowserRouter([
+  {
+    path: "/home",
+    element: <Home/>,
+  },
+  {
+      path: "/register",
+      element: <Register/>,
+  },  
+  {
+    path: "/login",
+    element: <Login/>,
+  },  
+  {
+    path: "/write",
+    element: <Write/>,
+  },  
+  {
+    path: "/single",
+    element: <Single/>,
+  },  
+]);
+
 function App() {
   return (
     <div className="App">
-      hola raton con cola!
+      <RouterProvider router={router}/>
     </div>
   );
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Login = () => {
   return (
     <div>
-      
+      xupale el choro a la Maria
     </div>
   )
 }
